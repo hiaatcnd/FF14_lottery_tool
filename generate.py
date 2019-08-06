8f83f012ab1a7d2b3a1372b381092871e7439090
huangxin <huangxin@megvii.com>
2019-08-06 17:24:30 +0800

    generate.py completed

--- generate.py
+++ generate.py
@@ -1,5 +1,40 @@
 import copy
 import itertools
+import json
+from IPython import embed
+
+lines = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)),
+         ((2, 0), (2, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+         ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)),
+         ((0, 0), (1, 1), (2, 2)), ((2, 0), (1, 1), (0, 2)))
+
+coins = (
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,  # 0-5: 0
+    10000,
+    36,
+    720,
+    360,
+    80,
+    252,
+    108,
+    72,
+    54,
+    180,
+    72,
+    180,
+    119,
+    36,
+    306,
+    1080,
+    144,
+    1800,
+    3600)
+
 
 def get_id(statu):
     the_id = 0
@@ -24,40 +59,93 @@ def get_remain_nums(statu):
     return tuple(remain_nums)
 
 
-def cal_max_expectation(statu):
+def cal_max_expectation(statu, output_flag=False):
     remain_nums = get_remain_nums(statu)
-    permu_it = itertools.permutations(remain_nums)
-    while True:
-        try:
-            permutation = next(permu_it)
-        except StopIteration:
-            break
 
-        
+    max_exp = -1
+    choose_line = ()
+    for line in lines:
+        cnt_blank = 0
+        nums = []
+        for x, y in line:
+            nums.append(statu[x][y])
+            if statu[x][y] == 0:
+                cnt_blank += 1
+
+        if cnt_blank == 0:
+            exp = coins[sum(nums)]
+            max_posiable = exp
+            min_posiable = exp
+        else:
+            permu_it = itertools.permutations(remain_nums, cnt_blank)
+            cnt_permu = 0
+            max_posiable = -1
+            min_posiable = 23333
+            tot_coins = 0
+            while True:
+                try:
+                    permutation = next(permu_it)
+                except StopIteration:
+                    break
+
+                cnt_permu += 1
+
+                permu_idx = 0
+                cnt_points = 0
+                for num in nums:
+                    if num == 0:
+                        cnt_points += permutation[permu_idx]
+                        permu_idx += 1
+                    else:
+                        cnt_points += num
+                max_posiable = max(max_posiable, coins[cnt_points])
+                min_posiable = min(min_posiable, coins[cnt_points])
+                tot_coins += coins[cnt_points]
+            exp = tot_coins / cnt_permu
+
+        if exp > max_exp:
+            max_exp = exp
+            choose_line = line
+
+        if output_flag:
+            line_real = [(x + 1, y + 1) for x, y in line]
+            print(
+                "line {} expectation: {:.2f}, max possible: {}, min possible: {}"
+                .format(str(line_real), exp, max_posiable, min_posiable))
+
+    if output_flag:
+        line_real = [(x + 1, y + 1) for x, y in choose_line]
+        print("Choose: " + str(line_real))
+        print("expectation of coins: %.2f" % max_exp)
+
+    return max_exp, choose_line
+
+
+dp = {}
 
 
 def search_next_step(statu, steps):
     statu_id = get_id(statu)
     if statu_id in dp:
-        return dp[statu_id]
+        return dp[statu_id][0]
 
     if steps >= 3:
-        tmp = (cal_max_expectation, -1, -1)
+        tmp = cal_max_expectation(statu)
         dp[statu_id] = tmp
-        return tmp
-    
+        return tmp[0]
+
     remain_nums = get_remain_nums(statu)
     max_expectation = -1
     step_x = -1
     step_y = -1
-    
+
     for i in range(3):
         for j in range(3):
             if statu[i][j] == 0:
                 avg_expectation = 0
                 for num in remain_nums:
                     statu[i][j] = num
-                    expectation, _, __ = search_next_step(statu, steps + 1)
+                    expectation = search_next_step(statu, steps + 1)
                     avg_expectation += expectation
                 avg_expectation /= len(remain_nums)
 
@@ -68,10 +156,12 @@ def search_next_step(statu, steps):
 
                 statu[i][j] = 0
 
-    tmp = (max_expectation, step_x, step_y)
-    dp[statu_id] = tmp
-    return tmp
+    dp[statu_id] = (max_expectation, step_x, step_y)
+    return max_expectation
+
 
 if __name__ == '__main__':
     statu = [[0 for j in range(3)] for i in range(3)]
-    search_next_step(statu)
+    search_next_step(statu, 0)
+    with open("data.json", "w") as f:
+        json.dump(dp, f)
